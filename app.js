e387d7dcaea9ee9809f64fcce0c43655dde53b81
Lawrance <lawchen522@gmail.com>
2024-03-07 10:23:16 +0800

    router init

--- app.js
+++ app.js
@@ -6,6 +6,34 @@ app.get('/', (req, res) => {
   res.send('hello word')
 })
 
+app.get('/restaurants', (req, res) => {
+  res.send('all restaurants')
+})
+
+app.get('/restaurants/new', (req, res) => {
+  res.send('new restaurant')
+})
+
+app.get('/restaurants/:id', (req, res) => {
+  res.send(`restaurant: ${req.params.id}`)
+})
+
+app.get('/restaurants/:id/edit', (req, res) => {
+  res.send(`get restaurant edit page: ${req.params.id}`)
+})
+
+app.post('/restaurants', (req, res) => {
+  res.send('add restaurant')
+})
+
+app.put('/restaurants/:id', (req, res) => {
+  res.send(`edit restaurant: ${req.params.id}`)
+})
+
+app.delete('/restaurants/:id', (req, res) => {
+  res.send(`delete restaurant: ${req.params.id}`)
+})
+
 app.listen(port, () => {
-  console.log(`App is running on http://localhost${port}`)
+  console.log(`App is running on http://localhost:${port}`)
 })
