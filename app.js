ccafd01564391b58620f49db41adb093b2667876
Lawrance <lawchen522@gmail.com>
2024-03-11 17:09:44 +0800

    implement updating restaurant

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 const { engine } = require('express-handlebars')
+const methodOverride = require('method-override')
 
 const db = require('./models')
 const Restaurant = db.Restaurants
@@ -11,6 +12,7 @@ app.set('view engine', '.hbs')
 app.set('views', './views')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true}))
+app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
   res.render('index')
@@ -40,27 +42,26 @@ app.get('/restaurants/:id', (req, res) => {
 })
 
 app.get('/restaurants/:id/edit', (req, res) => {
-  res.send(`get restaurant edit page: ${req.params.id}`)
+  const id = req.params.id
+
+  return Restaurant.findByPk(id, {
+    raw: true
+  })
+  .then((restaurant) => res.render('edit', {restaurant}))
 })
 
 app.post('/restaurants', (req, res) => {
   const name = req.body.name
-  const name_en = req.body.name_en
   const category = req.body.category
-  const image = req.body.image
   const location = req.body.location
   const phone = req.body.phone
-  const google_map = req.body.google_map
   const rating = Number(req.body.rating)
   const description = req.body.description
   return Restaurant.create({
     name,
-    name_en,
     category,
-    image,
     location,
     phone,
-    google_map,
     rating,
     description
   })
@@ -69,7 +70,17 @@ app.post('/restaurants', (req, res) => {
 })
 
 app.put('/restaurants/:id', (req, res) => {
-  res.send(`edit restaurant: ${req.params.id}`)
+  const id = req.params.id
+  const body = req.body
+  return Restaurant.update({
+    name: body.name,
+    category: body.category,
+    location: body.location,
+    phone: body.phone,
+    rating: body.rating,
+    description: body.description
+  }, { where: { id }})
+  .then(() => res.redirect(`/restaurants/${id}`))
 })
 
 app.delete('/restaurants/:id', (req, res) => {
